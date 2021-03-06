e3a48fb76eaef8399be3900efe9cae213b57ab8f
Nicolas Justin <nicolasjustin.dev@gmail.com>
2021-03-06 17:12:51 -0300

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home page</title>
+      </Head>
+
+      <main>
+        <h1>Home page</h1>
+      </main>
+    </>
+  );
+}
+
+export default Home;
