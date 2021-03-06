cfb407a4b8a83433a4232b6e0d29d7d64980d934
Nicolas Justin <nicolasjustin.dev@gmail.com>
2021-03-06 19:09:21 -0300

    chore: add styled-components and styled-media-query

---
+++ src/pages/_document.js
@@ -0,0 +1,47 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () => {
+        const enhanceApp = (App) => {
+          return (props) => sheet.collectStyles(<App {...props} />);
+        };
+
+        return originalRenderPage({ enhanceApp });
+      };
+
+      const initialProps = await Document.getInitialProps(ctx);
+
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render() {
+    return (
+      <Html lang="en-US">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
