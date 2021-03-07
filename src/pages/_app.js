1c7f276f2bc0cb33c8ec349b12d5e6c8d128f33d
Nicolas Justin <nicolasjustin.dev@gmail.com>
2021-03-06 21:41:14 -0300

    chore: add normalize.css

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,25 @@
+import Head from 'next/head';
+import PropTypes from 'prop-types';
+
+import 'normalize.css';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="theme-color" content="#fff" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
 
 export default MyApp;
