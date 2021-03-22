667a7cfae047dc4017f859335e516e0cbfc77c47
Nicolas Justin <nicolasjustin.dev@gmail.com>
2021-03-21 23:20:56 -0300

    feat: add services page

---
+++ src/pages/services.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+import ServicesTemplate from 'templates/Services';
+
+function Services() {
+  return (
+    <>
+      <Head>
+        <title>Services: Woofs &amp; Purrs</title>
+      </Head>
+
+      <ServicesTemplate />
+    </>
+  );
+}
+
+export default Services;
---
+++ src/templates/Services/index.js
@@ -0,0 +1,13 @@
+import HeroSection from './HeroSection';
+
+import * as S from './styles';
+
+function Services() {
+  return (
+    <S.Wrapper>
+      <HeroSection />
+    </S.Wrapper>
+  );
+}
+
+export default Services;
